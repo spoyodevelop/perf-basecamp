b03c9369de22c01ce8198a03a481ec834a416623
spoyodevelop <kjy9505@gmail.com>
2025-09-03 21:52:51 +0900

    refactor: add Suspense for lazy loading of Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
-
-const Home = lazy(() => import('./pages/Home/Home'));
-const Search = lazy(() => import('./pages/Search/Search'));
+import { lazy, Suspense } from 'react';
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-
+import Home from './pages/Home/Home';
 import './App.css';
+
+const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );

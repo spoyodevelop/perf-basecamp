f58cf9176c7683135ae585b8bc164e72a0d4de17
spoyodevelop <kjy9505@gmail.com>
2025-09-03 17:56:01 +0900

    refactor: 코드 스플리팅을 통한 퍼포먼스 개선

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
 
-import Home from './pages/Home/Home';
-import Search from './pages/Search/Search';
+const Home = lazy(() => import('./pages/Home/Home'));
+const Search = lazy(() => import('./pages/Search/Search'));
 
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';

2e6b2dc9e9101bd422d273317b15f3901aae0901
spoyodevelop <kjy9505@gmail.com>
2025-09-05 15:50:27 +0900

    refactor: virtualized list 구현 및 적용

--- src/pages/Search/components/ArtistInfo/ArtistInfo.tsx
+++ src/pages/Search/components/ArtistInfo/ArtistInfo.tsx
@@ -4,13 +4,14 @@ import styles from './ArtistInfo.module.css';
 
 export type ArtistProps = {
   artist: Artist;
+  style: React.CSSProperties;
 };
 
-const ArtistInfo = ({ artist }: ArtistProps) => {
+const ArtistInfo = ({ artist, style }: ArtistProps) => {
   const { name, profileUrl, profileImageUrl } = artist;
 
   return (
-    <li className={styles.artistContainer}>
+    <li className={styles.artistContainer} style={style}>
       <img className={styles.profileImage} src={profileImageUrl} />
       <p>
         <a className={styles.profileUrl} href={profileUrl}>
